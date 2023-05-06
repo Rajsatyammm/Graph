9cde5524dd2a22ceede3ffabd9292d8db6f5ce98
Satyam Raj <rajsatyammm@gmail.com>
2023-05-06 12:01:56 +0530

    Create hasPath.java

---
+++ hasPath.java
@@ -0,0 +1,13 @@
+public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis) { // T(n) = O(V+E)
+        if (src == dest) {
+            return true;
+        }
+        vis[src] = true;
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if (!vis[e.dest] && hasPath(graph, e.dest, dest, vis)) {
+                return true;
+            }
+        }
+        return false;
+    }
