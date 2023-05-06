eeb9b9e8f47c5c5df41b2ec0698af9264b77d52e
Satyam Raj <rajsatyammm@gmail.com>
2023-05-06 11:58:08 +0530

    Create Graph Node.java

---
+++ Graph Node.java
@@ -0,0 +1,11 @@
+static class Edge {
+        int src;
+        int dest;
+        int wt;
+
+        Edge(int src, int dest, int wt) {
+            this.src = src;
+            this.dest = dest;
+            this.wt = wt;
+        }
+    }
