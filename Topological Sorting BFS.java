9fa1573f3ddec6a657a681ef01a65a066967c14e
Satyam Raj <rajsatyammm@gmail.com>
2023-05-21 00:14:32 +0530

    Create Topological Sorting BFS.java

---
+++ Topological Sorting BFS.java
@@ -0,0 +1,35 @@
+public static void calcIndegree(ArrayList<Edge>[] graph, int[] indeg) {
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
+            }
+        }
+    }
+
+    public static void topSortBFS(ArrayList<Edge>[] graph) { // Using BFS (Khan's Algo)
+        int[] indeg = new int[graph.length];
+        calcIndegree(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            System.out.print(curr + " ");
+
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
+    }
